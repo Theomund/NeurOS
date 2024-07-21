aedb42a45e39e6994d7dd8eecb08bbeeb49e3561
Theomund <34360334+Theomund@users.noreply.github.com>
2024-07-21 06:00:07 +0000

    feat: add header and file structure definitions

--- src/kernel/initrd.zig
+++ src/kernel/initrd.zig
@@ -21,6 +21,30 @@ const Log = std.log.scoped(.initrd);
 
 pub export var module_request: limine.ModuleRequest = .{};
 
+const Header = struct {
+    name: [100]u8,
+    mode: [8]u8,
+    uid: [8]u8,
+    gid: [8]u8,
+    size: [12]u8,
+    mtime: [12]u8,
+    checksum: [8]u8,
+    flag: u8,
+    linked: [100]u8,
+    indicator: [6]u6,
+    version: [2]u8,
+    username: [32]u8,
+    group: [32]u8,
+    major: [8]u8,
+    minor: [8]u8,
+    prefix: [155]u8,
+};
+
+const File = struct {
+    header: Header,
+    data: *const u8,
+};
+
 pub fn init() void {
     if (module_request.response) |module_response| {
         const initrd = module_response.modules()[0];
