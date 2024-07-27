b55f5df6567f117fd9f40b3b9e7623f848d777d6
Theomund <34360334+Theomund@users.noreply.github.com>
2024-07-27 19:47:19 +0000

    feat: create VGA display struct

--- src/kernel/vga.zig
+++ src/kernel/vga.zig
@@ -20,60 +20,43 @@ const std = @import("std");
 
 const Log = std.log.scoped(.vga);
 
-const Context = struct { bold: bool, x: usize, y: usize, fg: u32, bg: u32 };
-const WriteError = error{FileNotFound};
-pub const Writer = std.io.GenericWriter(Context, WriteError, write);
-
 pub export var framebuffer_request: limine.FramebufferRequest = .{};
-pub var framebuffer: *limine.Framebuffer = undefined;
 
-pub fn init() !void {
-    try setupFramebuffer();
-    try printMessage();
-    Log.info("Initialized the VGA subsystem.", .{});
-}
+const Display = struct {
+    framebuffer: *limine.Framebuffer,
 
-fn setupFramebuffer() !void {
-    if (framebuffer_request.response) |framebuffer_response| {
-        framebuffer = framebuffer_response.framebuffers()[0];
-    } else {
-        Log.err("Failed to retrieve a framebuffer response.", .{});
-        return error.MissingFramebuffer;
-    }
-}
-
-fn printMessage() !void {
-    const writer = Writer{ .context = .{
-        .bold = true,
-        .x = 0,
-        .y = 16,
-        .fg = 0xFFFF00,
-        .bg = 0x000000,
-    } };
-    try writer.print("Hello, world!", .{});
-}
-
-fn drawPixel(x: usize, y: usize, color: u32) void {
-    const offset = y * framebuffer.pitch + x * 4;
-    @as(*u32, @ptrCast(@alignCast(framebuffer.address + offset))).* = color;
-}
-
-fn drawCharacter(face: font.Face, character: u8, x: usize, y: usize, fg: u32, bg: u32) void {
-    const width: usize = face.getWidth();
-    const height: usize = face.getHeight();
-
-    const position: usize = character * height;
-    const glyph: []const u8 = face.data[position..];
-
-    const masks: [8]u8 = .{ 128, 64, 32, 16, 8, 4, 2, 1 };
-
-    for (0..height) |cy| {
-        for (0..width) |cx| {
-            const color = if (glyph[cy] & masks[cx] == 0) bg else fg;
-            drawPixel(cx + x, cy + y - 12, color);
+    fn init() !Display {
+        if (framebuffer_request.response) |framebuffer_response| {
+            const fb = framebuffer_response.framebuffers()[0];
+            return Display{ .framebuffer = fb };
+        } else {
+            Log.err("Failed to retrieve a framebuffer response.", .{});
+            return error.MissingFramebuffer;
         }
     }
-}
+
+    fn drawPixel(self: Display, x: usize, y: usize, color: u32) void {
+        const offset = y * self.framebuffer.pitch + x * 4;
+        @as(*u32, @ptrCast(@alignCast(self.framebuffer.address + offset))).* = color;
+    }
+
+    fn drawCharacter(self: Display, face: font.Face, character: u8, x: usize, y: usize, fg: u32, bg: u32) void {
+        const width: usize = face.getWidth();
+        const height: usize = face.getHeight();
+
+        const position: usize = character * height;
+        const glyph: []const u8 = face.data[position..];
+
+        const masks: [8]u8 = .{ 128, 64, 32, 16, 8, 4, 2, 1 };
+
+        for (0..height) |cy| {
+            for (0..width) |cx| {
+                const color = if (glyph[cy] & masks[cx] == 0) bg else fg;
+                self.drawPixel(cx + x, cy + y - 12, color);
+            }
+        }
+    }
+};
 
 fn write(context: Context, bytes: []const u8) WriteError!usize {
     const face = if (context.bold) try font.Face.init("./usr/share/fonts/ter-i16b.psf") else try font.Face.init("./usr/share/fonts/ter-i16n.psf");
@@ -93,11 +76,33 @@ fn write(context: Context, bytes: []const u8) WriteError!usize {
                 x = context.x;
             },
             else => {
-                drawCharacter(face, byte, x, y, context.fg, context.bg);
+                context.display.drawCharacter(face, byte, x, y, context.fg, context.bg);
                 x += width;
             },
         }
     }
 
     return bytes.len;
+}
+
+const Context = struct { display: Display, bold: bool, x: usize, y: usize, fg: u32, bg: u32 };
+const WriteError = error{FileNotFound};
+pub const Writer = std.io.GenericWriter(Context, WriteError, write);
+
+fn printMessage() !void {
+    const display = try Display.init();
+    const writer = Writer{ .context = .{
+        .display = display,
+        .bold = true,
+        .x = 0,
+        .y = 16,
+        .fg = 0xFFFF00,
+        .bg = 0x000000,
+    } };
+    try writer.print("Hello, world!", .{});
+}
+
+pub fn init() !void {
+    try printMessage();
+    Log.info("Initialized the VGA subsystem.", .{});
 }
