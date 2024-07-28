1b92c6bc8b53ae324233356f7b41ad1b3b297a0a
Theomund <34360334+Theomund@users.noreply.github.com>
2024-07-28 08:49:05 +0000

    feat: create process subsystem

---
+++ src/kernel/process.zig
@@ -0,0 +1,48 @@
+// NeurOS - Hobbyist operating system written in Zig.
+// Copyright (C) 2024 Theomund
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+const State = enum {
+    Running,
+    Stopped,
+};
+
+const Context = struct {
+    rax: u64,
+    rbx: u64,
+    rcx: u64,
+    rdx: u64,
+    rsi: u64,
+    rdi: u64,
+    rsp: u64,
+    rbp: u64,
+    r8: u64,
+    r9: u64,
+    r10: u64,
+    r11: u64,
+    r12: u64,
+    r13: u64,
+    r14: u64,
+    r15: u64,
+    rip: u64,
+    rflags: u64,
+};
+
+const Task = struct {
+    id: u64,
+    context: Context,
+    name: []const u8,
+    state: State,
+};
