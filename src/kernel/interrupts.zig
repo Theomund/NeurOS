34b9d65a8fc1801ab2eaf60ec2f85aea9c39b935
Theomund <34360334+Theomund@users.noreply.github.com>
2024-08-04 03:07:40 +0000

    feat: add interrupt descriptor table structure

--- src/kernel/interrupts.zig
+++ src/kernel/interrupts.zig
@@ -18,6 +18,36 @@ const std = @import("std");
 
 const Log = std.log.scoped(.interrupts);
 
+const Entry = packed struct {
+    offset_low: u16,
+    selector: u16,
+    ist: u8,
+    type_attributes: u8,
+    offset_middle: u16,
+    offset_high: u32,
+    zero: u32,
+};
+
+const InterruptDescriptorTable = struct {
+    entries: [256]Entry,
+
+    fn init() void {}
+
+    fn makeEntry(offset: u64, selector: u16, ist: u8, type_attributes: u8) Entry {
+        return .{
+            .offset_low = @truncate(offset),
+            .selector = selector,
+            .ist = ist,
+            .type_attributes = type_attributes,
+            .offset_middle = @truncate(offset << 16),
+            .offset_high = @truncate(offset << 32),
+            .zero = 0,
+        };
+    }
+};
+
+const InterruptHandler = fn () callconv(.Interrupt) void;
+
 pub fn init() void {
     enable();
     Log.info("Initialized the interrupts subsystem.", .{});
